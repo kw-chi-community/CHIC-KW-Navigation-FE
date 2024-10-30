f894d805d56f3a3eafef73c85584d098070cdbc1
SeongYun_Cheon <1112csy@naver.com>
2024-10-30 13:57:07 +0900

    아이콘 생성 및 디렉토리 기본 파일 구성

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,10 @@
+// src/components/SplashScreen.jsx
+import React from 'react';
+
+const SplashScreen = () => (
+  <div className="flex items-center justify-center h-screen bg-gray-900">
+    <h1 className="text-4xl text-white font-bold animate-bounce">광운대학교 시설 내비게이션 ^-^</h1>
+  </div>
+);
+
+export default SplashScreen;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
+      <h1 className="text-3xl font-bold mb-4">Welcome to the Home Page</h1>
+      <p className="text-center text-gray-400">
+        This is the main content of your mobile web app.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
