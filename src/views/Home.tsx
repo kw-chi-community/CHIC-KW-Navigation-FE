c787bb687b9ac90db2b836f8387f9ab72c9ad4ee
SeongYun_Cheon <1112csy@naver.com>
2024-12-21 17:38:31 +0900

    ✨: 비회원 광운대 모달 연결 (임시 레이아웃웃)

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+import ExModal from "../components/Modal/ExModal/ExModal";
+
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="container h-[100vh] flex flex-col items-center justify-center bg-gradient-to-br from-red-700 to-red-950 text-white">
-      <h1 className="text-3xl font-bold mb-6 animate-bounce">
-        광운 내비게이션
-      </h1>
-      <button className="bg-yellow-400 text-black px-6 py-2 rounded-lg shadow-lg hover:bg-yellow-300 hover:rotate-3 scale-105 transition-all duration-300 mb-4">
-        딸깍 !
+      <h1 className="text-3xl font-bold mb-6 animate-bounce">광운 내비게이션</h1>
+
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="bg-pink-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-pink-400 hover:rotate-3 hover:scale-105 transition-all duration-300"
+      >
+        모달 열기
       </button>
-      <button className="bg-pink-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-pink-400 hover:rotate-3 hover:scale-105 transition-all duration-300">
-        딸깍 !!
-      </button>
+      <ExModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   );
 };
