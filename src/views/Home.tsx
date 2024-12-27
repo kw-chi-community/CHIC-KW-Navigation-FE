caefe2999f8370a3070b123f060ae6756d4a7e85
seongyun4359 <4342csy@gmail.com>
2024-12-27 10:57:43 +0900

    🎨:  실습용 디자인 변경

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import ExModal from "../components/Modal/ExModal/ExModal";
+import Header from "@/components/Header/Header";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="container h-[100vh] flex flex-col items-center justify-center bg-gradient-to-br from-red-700 to-red-950 text-white">
-      <h1 className="text-3xl font-bold mb-6 animate-bounce">광운 내비게이션</h1>
+    <div className="container h-[100vh] flex flex-col items-center justify-center bg-white text-white">
+      <Header />
+      <h1 className="text-3xl font-bold mb-6 animate-bounce text-black">광운 내비게이션</h1>
 
       <button
         onClick={() => setIsModalOpen(true)}
-        className="bg-pink-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-pink-400 hover:rotate-3 hover:scale-105 transition-all duration-300"
+        className="bg-red-800 text-white px-6 py-2 rounded-lg shadow-lg  hover:rotate-3 hover:scale-105 transition-all duration-300"
       >
         모달 열기
       </button>
