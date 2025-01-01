24444e02278f4c33d8d0712ebd05f9dc3ccce601
장윤선 <dbstjs2587@naver.com>
2025-01-02 01:46:20 +0900

    장소 검색 페이지 검색 아이콘, 검색 창 디자인

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,30 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, Search } from "lucide-react";
 
 function Header() {
+  const buttonClass = "flex items-center space-x-2 mr-2";
+
   return (
     <div className="container fixed top-0 left-0 right-0 h-[7vh] bg-[#7D170A] flex items-center justify-between px-4 text-white shadow-md z-20">
 
       <button
         onClick={() => window.history.back()}
-        className="flex items-center space-x-2 mr-2"
+        className={buttonClass}
       >
         <ChevronLeft color="#ffffff" />
       </button>
 
-      <h1 className="text-lg font-semibold">현재 페이지</h1>
-      <div className="w-8"></div>
+      <div className="relative flex items-center">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full h-[40px] rounded-full bg-white text-black text-center px-4 shadow-md focus:outline-none"
+          />
+      </div>
+
+      <button className={buttonClass}>
+        <Search color="#ffffff" />
+      </button>
+      
     </div>
   );
 }
