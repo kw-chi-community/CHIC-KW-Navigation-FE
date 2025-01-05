f135bbd335d34d2bf4a8380c0b929ee5e136c758
장윤선 <dbstjs2587@naver.com>
2025-01-06 02:56:36 +0900

    🧹: App.tsx 파일에 Login.tsx 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Splash from "./views/Splash";
 import Home from "./views/Home";
+import Login from "./views/Login";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<Splash />} />
         {/* 홈 화면 */}
         <Route path="/Home" element={<Home />} />
+        {/* 로그인 화면 */}
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
