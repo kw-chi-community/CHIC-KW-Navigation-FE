6e59c7af650a03f84aaae008fa2e8d061c2ec939
천성윤 <1112csy@naver.com>
2025-01-20 15:44:23 +0900

    🔥: 불필요 예제 코드 삭제

--- src/views/RouteQuestion/QuestionPage.tsx
+++ src/views/RouteQuestion/QuestionPage.tsx
@@ -1,18 +1,7 @@
-import ComButton from '@/components/common/Button/ComButton/ComButton';
-
 export default function QuestionPage() {
   return (
     
       <div className="container h-auto bg-white text-black">
-
-       <ComButton size='xl' > ㅎㅇ</ComButton>
-
-       <ComButton size='l' color='gray' > ㅎㅇ</ComButton>
-
-       <ComButton size='xl' color='black' > ㅎㅇ</ComButton>
-
-
-       <ComButton > ㅎㅇ</ComButton>
     </div>
   )
 }
