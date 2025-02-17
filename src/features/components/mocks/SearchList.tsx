a35fcdd3b6e97d6e54c9305d05c1839b16251674
장윤선 <dbstjs2587@naver.com>
2025-02-18 01:05:59 +0900

    🤡: 목적지 검색결과 리스트 mock 데이터 생성

---
+++ src/features/components/mocks/SearchList.tsx
@@ -0,0 +1,8 @@
+const searchPlaces = [
+    { name: "광운대학교 한울관", location: "서울특별시 월계1동" },
+    { name: "광운대학교 새빛관", location: "서울특별시 월계1동" },
+    { name: "광운대학교 비마관", location: "서울특별시 월계1동" },
+];
+  
+export default searchPlaces;
+  
